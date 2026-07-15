e4723b4e594c62ec94e0abf78a048de0f0e13c32
sanikatavare08-pixel <sanikatavare08@gmail.com>
2026-07-15 17:31:06 +0530

    Add files via upload

--- versions.py
+++ versions.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
-
 import streamlit as st
 import cv2
+import tensorflow as tf
+from importlib.metadata import version
 
-print(np.__version__)
-print(pd.__version__)
-
-print(st.__version__)
-print(cv2.__version__)
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("Streamlit:", st.__version__)
+print("OpenCV:", cv2.__version__)
+print("TensorFlow:", tf.__version__)
+print("Drawable Canvas:", version("streamlit-drawable-canvas"))
